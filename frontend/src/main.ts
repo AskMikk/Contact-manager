ed82ce1a5f75c8e4f34c9d4313a7022c6c8fe71d
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-01-18 18:04:44 +0200

    Frontend - initial changes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './app/enviroments/enviroment';
 import { AppModule } from './app/app.module';
 
+
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
